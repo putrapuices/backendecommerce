3dc559d97473e2d31f4d5bfdfb49d930e00b9616
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-19 22:28:25 +0700

    Accessor di dalam Model Category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,5 +19,16 @@ class Category extends Model
     public function products()
     {
         return $this->hasMany(Product::class);
+    }
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/categories/' . $value),
+        );
     }
 }
