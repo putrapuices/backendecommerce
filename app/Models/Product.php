da4aa850cc69536d454e349c9a6e76590373bacb
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-19 22:31:36 +0700

    Accessor di dalam Model product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -35,4 +35,28 @@ class Product extends Model
     {
         return $this->hasMany(Review::class);
     }
+    
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/products/' . $value),
+        );
+    }
+        
+    /**
+     * reviewAvgRating
+     *
+     * @return Attribute
+     */
+    public function reviewAvgRating(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value ? substr($value, 0, 3) : 0,
+        );
+    } 
 }
