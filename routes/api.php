38f2c66d904b6317b535f0abbbf51d0578e417ec
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-21 13:53:54 +0700

    Membuat CRUD API Sliders

--- routes/api.php
+++ routes/api.php
@@ -47,5 +47,7 @@ Route::prefix('admin')->group(function () {
         Route::apiResource('/invoices', App\Http\Controllers\Api\Admin\InvoiceController::class, ['except' => ['create', 'store', 'edit', 'update', 'destroy'], 'as' => 'admin']);
         //customer
         Route::get('/customers', [App\Http\Controllers\Api\Admin\CustomerController::class, 'index', ['as' => 'admin']]);
+        //sliders resource
+Route::apiResource('/sliders', App\Http\Controllers\Api\Admin\SliderController::class, ['except' => ['create', 'show', 'edit', 'update'], 'as' => 'admin']);
     });
 });
