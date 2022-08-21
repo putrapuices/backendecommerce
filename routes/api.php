b2fe2a3420aa43225da7d9681c9104b443cc423b
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-21 14:02:41 +0700

    Membuat CRUD API Users

--- routes/api.php
+++ routes/api.php
@@ -49,5 +49,7 @@ Route::prefix('admin')->group(function () {
         Route::get('/customers', [App\Http\Controllers\Api\Admin\CustomerController::class, 'index', ['as' => 'admin']]);
         //sliders resource
 Route::apiResource('/sliders', App\Http\Controllers\Api\Admin\SliderController::class, ['except' => ['create', 'show', 'edit', 'update'], 'as' => 'admin']);
+//users resource
+Route::apiResource('/users', App\Http\Controllers\Api\Admin\UserController::class, ['except' => ['create', 'edit'], 'as' => 'admin']);
     });
 });
