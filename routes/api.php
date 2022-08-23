65565ed6ef3a0cb0c8e424f43ade8e70044973c6
Rahmatputra <rahmatputrankri@gmail.com>
2022-08-23 09:59:16 +0700

    Membuat Restful API Carts

--- routes/api.php
+++ routes/api.php
@@ -99,5 +99,19 @@ Route::get('/rajaongkir/provinces', [App\Http\Controllers\Api\Web\RajaOngkirCont
 Route::post('/rajaongkir/cities', [App\Http\Controllers\Api\Web\RajaOngkirController::class, 'getCities'], ['as' => 'web']);
 
 Route::post('/rajaongkir/checkOngkir', [App\Http\Controllers\Api\Web\RajaOngkirController::class, 'checkOngkir'], ['as' => 'web']);
+//get cart 
+Route::get('/carts', [App\Http\Controllers\Api\Web\CartController::class, 'index'], ['as' => 'web']);
+
+//store cart
+Route::post('/carts', [App\Http\Controllers\Api\Web\CartController::class, 'store'], ['as' => 'web']);
+    
+//get cart price
+Route::get('/carts/total_price', [App\Http\Controllers\Api\Web\CartController::class, 'getCartPrice'], ['as' => 'web']);
+
+//get cart weight
+Route::get('/carts/total_weight', [App\Http\Controllers\Api\Web\CartController::class, 'getCartWeight'], ['as' => 'web']);
+
+//remove cart
+Route::post('/carts/remove', [App\Http\Controllers\Api\Web\CartController::class, 'removeCart'], ['as' => 'web']);
 
 });
